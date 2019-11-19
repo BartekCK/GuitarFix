45170cc91e209516c43799c769e5e769d3b1a057
Bartek <bkotarski1@gmail.com>
2019-11-19 16:18:02 +0100

    Add form Controller

---
+++ controllers/FormController.js
@@ -0,0 +1,4 @@
+exports.sendFrom = (req,res)=>{
+  console.log(req.body);
+  res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const pagesController = require('../controllers/PagesController');
+const formController = require('../controllers/FormController');
 
 router.get('/', pagesController.home);
 
-
+router.post('/send',formController.sendFrom);
 module.exports = router;
